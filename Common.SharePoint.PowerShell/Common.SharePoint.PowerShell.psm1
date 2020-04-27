468d5154e5b5e7aceb1ec9a859abbb62360f37c2
Judele, Daniel <judeih@yahoo.com>
2020-04-27 17:17:21 +0300

    export context function

--- Common.SharePoint.PowerShell/Common.SharePoint.PowerShell.psm1
+++ Common.SharePoint.PowerShell/Common.SharePoint.PowerShell.psm1
@@ -1,3 +1,15 @@
+if (!(Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable)) {
+    Install-Module -Name Microsoft.Online.SharePoint.PowerShell  -Confirm
+}
+
+if (!(Get-Module -Name SharePointPnPPowerShellOnline -ListAvailable)) {
+    Install-Module -Name SharePointPnPPowerShellOnline  -Confirm
+}
+
+Import-Module Microsoft.Online.SharePoint.PowerShell
+Import-Module SharePointPnPPowerShellOnline
+
 ."$PSScriptRoot\Site Script\*.ps1"
+."$PSScriptRoot\Context\*.ps1"
 
 Export-ModuleMember -Function *
